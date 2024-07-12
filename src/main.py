b5658e0b0108a40dead061264ac5b77712ed0f1d
rafaelMachado1891 <rafadmn1@gmail.com>
2024-07-11 23:40:52 -0300

    carregando as datas dos eventos no dicionario

--- src/main.py
+++ src/main.py
@@ -1,17 +1,27 @@
 import requests
-from bs4 import  BeautifulSoup
+from bs4 import BeautifulSoup
 import pandas as pd
 
 url = "https://verdictmma.com/fighter/alex-poatan-pereira"
 
 response = requests.get(url)
 if response.status_code == 200: 
- 
- soup = BeautifulSoup(response.text, 'html.parser')
+    soup = BeautifulSoup(response.text, 'html.parser')
 
- resultado = soup.find_all("div", class_="relative w-full justify-center")
- print(resultado)
+    resultado = soup.find_all("div", class_="flex-grow")
+
+    data = []
+    for dados in resultado:
+        # Encontrar a div com a classe text-sm dentro do elemento atual
+        data_evento_div = dados.find("div", class_="text-sm text-gray-500 leading-none")
+        if data_evento_div:
+            # Extrair o texto do elemento encontrado
+            data_evento = data_evento_div.text.strip()
+        else:
+            data_evento = None  # Ou você pode usar um valor padrão como uma string vazia
+
+        data.append({'Data_Evento': data_evento})
+
+    print(data)
 else:
-  print("erro ao conectar no site")
-
-
+    print("erro ao conectar no site")

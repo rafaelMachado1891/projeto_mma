952599e1571a28be84da1a60c951d6bd97e5755a
rafaelMachado1891 <rafadmn1@gmail.com>
2024-07-14 11:31:47 -0300

    raspagem dos dados e cricao do arquivo parquet

--- src/extract.py
+++ src/extract.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+
 def get_text_or_none(soup_element, tag, class_name):
     element = soup_element.find(tag, class_=class_name)
     return element.text.strip() if element else None
@@ -19,9 +20,18 @@ if response.status_code == 200:
         # Usar a função auxiliar para encontrar e extrair o texto
         data_evento = get_text_or_none(dados, "div", "text-sm text-gray-500 leading-none")
         evento = get_text_or_none(dados, "div", "text-sm font-medium font-sans text-gray-500 text-right leading-none")
+        desafiante = get_text_or_none(dados, "div","mt-2 text-base font-medium text-gray-800 font-sans leading-none")
+        resultado = get_text_or_none(dados, "span","text-sm font-bold text-green-500 font-sans leading-none")
+        round = get_text_or_none(dados, "span", "text-sm text-gray-500 leading-none")
 
-        data.append({'Data_Evento': data_evento, "Card": evento})
+        data.append({'Data_Evento': data_evento, "Evento": evento, "Desafiante": desafiante,"resultado":resultado, "round": round})
 
-    print(data)
+    
 else:
     print("erro ao conectar no site")
+
+df = pd.DataFrame(data=data)
+
+print(df)
+
+df.to_parquet('dados_df.parquet', index=False)

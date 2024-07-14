61ce0d89d68f4b09e57ede58bf6c35910f3f8e3d
rafaelMachado1891 <rafadmn1@gmail.com>
2024-07-14 18:02:00 -0300

    Finalizando commit

--- src/extract.py
+++ src/extract.py
@@ -13,19 +13,22 @@ response = requests.get(url)
 if response.status_code == 200: 
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    resultado = soup.find_all("div", class_="flex-grow")
+    lutador = get_text_or_none(soup,"h1", "text-gray-800 text-base md:text-xl font-bold font-sans")
+    
+    lutas = soup.find_all("div", class_="flex-grow")
 
     data = []
-    for dados in resultado:
+    for dados in lutas:
         # Usar a função auxiliar para encontrar e extrair o texto
+        #lutador = get_text_or_none(lutador, "h1", "text-gray-800 text-base md:text-xl font-bold font-sans")
         data_evento = get_text_or_none(dados, "div", "text-sm text-gray-500 leading-none")
         evento = get_text_or_none(dados, "div", "text-sm font-medium font-sans text-gray-500 text-right leading-none")
         desafiante = get_text_or_none(dados, "div","mt-2 text-base font-medium text-gray-800 font-sans leading-none")
         resultado = get_text_or_none(dados, "span","text-sm font-bold text-green-500 font-sans leading-none")
         round = get_text_or_none(dados, "span", "text-sm text-gray-500 leading-none")
 
-        data.append({'Data_Evento': data_evento, "Evento": evento, "Desafiante": desafiante,"resultado":resultado, "round": round})
-
+        data.append({"Lutador": lutador,"Data_Evento": data_evento, "Evento": evento, "Desafiante": desafiante,"resultado":resultado, "round": round})
+# 
     
 else:
     print("erro ao conectar no site")
